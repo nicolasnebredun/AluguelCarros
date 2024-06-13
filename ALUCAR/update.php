6c36dd74ac44a6bb9136479af7957ffc59fb0bc3
NicolasNeto <nicolasd1545@gmail.com>
2024-06-13 00:06:12 -0400

    Tela de Loading pra update e Delete

--- ALUCAR/update.php
+++ ALUCAR/update.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style/loader.css">
 </head>
 
 <body>
-    <h1>dwadwa</h1>
+    <div class="loader"> Atualizando...</div>
+    <div id="myProgress">
+        <div id="myBar"></div>
+    </div>
 </body>
 
 </html>
