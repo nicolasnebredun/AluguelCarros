abb23a693ed53eab610dc4423aaad74de3edbf7d
unknown <sinvaldopissinatti2@gmail.com>
2024-06-14 20:14:08 -0400

    arrrumei a página de listagem de carros disponíveis, e coloquei um pop-up com formulário, mas ainda estou ajeitando

--- ALUCAR/carrosParaAlugar.php
+++ ALUCAR/carrosParaAlugar.php
@@ -17,7 +17,6 @@ require 'conexao.php';
 </head>
 
 <body>
-
   </div>
   <div class="logo-img">
     <img src="./assets/LOGO SIMBOLO - BRANCO.svg" alt="logo cor branco limao" />
@@ -42,31 +41,143 @@ require 'conexao.php';
         <li><a href="index.html">Sair</a></li>
       </ul>
 
-    <header>
-    <div class="logo">
-    </div>
-  </header>
-    <div class="welcome-section">
-        <h1>Carros disponíveis para aluguel!</h1>
-    </div>
-    <div class="car-section">
-        <div class="car-box">
-            <img src="./assets/Rectangle 5.png" alt="Fiat uno - 1.6">
-            <h2>Fiat uno - 1.6</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <button>Alugar este carro</button>
+      <header>
+        <div class="logo">
         </div>
-        <div class="car-box">
-            <img src="./assets/Rectangle 5-1.png" alt="Fiat uno - 1.6">
-            <h2>Fiat uno - 1.6</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <button>Alugar este carro</button>
+      </header>
+      <div class="mainThings">
+        <div class="welcome-section">
+          <h1>Carros disponíveis para aluguel!</h1>
         </div>
-        <div class="car-box">
-            <img src="./assets/Rectangle 5-2.png" alt="Fiat uno - 1.6">
-            <h2>Fiat uno - 1.6</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <button>Alugar este carro</button>
+        <div class="car-section">
+          <div class="car-box">
+            <div class="carImages">
+              <img src="./assets/Fiat-Uno-16-mpi.png" alt="Fiat uno - 1.6">
+            </div>
+            <div class="texts">
+              <h2 class="car-title">Fiat uno - 1.6</h2>
+              <p class="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <button class="car-button">Alugar este carro</button>
+            </div>
+          </div>
+          <div class="car-box">
+            <div class="carImages">
+              <img src="./assets/fiat-strada-2021-cabine-simples.png" alt="Strada - 2.0">
+            </div>
+            <div class="texts">
+              <h2 class="car-title">Strada - 2.0</h2>
+              <p class="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <button class="car-button">Alugar este carro</button>
+            </div>
+          </div>
+          <div class="car-box">
+            <div class="carImages">
+              <img src="./assets/Rectangle 5-2.png" alt="Fiat uno - 1.6">
+            </div>
+            <div class="texts">
+              <h2 class="car-title">Fiat uno - 1.6</h2>
+              <p class="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <button class="car-button">Alugar este carro</button>
+            </div>
+          </div>
+          <div class="car-box">
+            <div class="carImages">
+              <img src="./assets/Fiat-Uno-16-mpi.png" alt="Fiat uno - 1.6">
+            </div>
+            <div class="texts">
+              <h2 class="car-title">Fiat uno - 1.6</h2>
+              <p class="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <button class="car-button">Alugar este carro</button>
+            </div>
+          </div>
+          <div class="car-box">
+            <div class="carImages">
+              <img src="./assets/fiat-strada-2021-cabine-simples.png" alt="Strada - 2.0">
+            </div>
+            <div class="texts">
+              <h2 class="car-title">Strada - 2.0</h2>
+              <p class="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <button class="car-button">Alugar este carro</button>
+            </div>
+          </div>
+          <div class="car-box">
+            <div class="carImages">
+              <img src="./assets/Fiat-Argo1.0-2023-1.png" alt="Fiat Argo - 1.0">
+            </div>
+            <div class="texts">
+              <h2 class="car-title">Fiat Argo - 1.0</h2>
+              <p class="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <button class="car-button" onclick="openModal()">Alugar este carro</button>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+      <div class="modal-container">
+        <?php
+        if (!isset($_POST['nome']) || !isset($_POST['cpf']) || !isset($_POST['telefone']) || !isset($_POST['qDias']) || !isset($_POST['dRetirada']) || !isset($_POST['dDevolucao']) || !isset($_POST['lRetirada']) || !isset($_POST['lDevolucao']) || !isset($_POST['hRetirada']) || !isset($_POST['hDevolucao']) || !isset($_POST['carro'])) {
+          echo '<div class="modal">';
+          echo '<h2>Preencha o formulário para alugar!</h2>';
+          echo '<hr>';
+          echo '<form action="conexao.php" method="post">';
+          echo '<div class="campo">';
+          echo '<label for="nome">Nome Completo:</label>';
+          echo '<input type="text" id="nome" name="nome" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="cpf">CPF:</label>';
+          echo '<input type="text" id="cpf" name="cpf" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="telefone">Telefone:</label>';
+          echo '<input type="tel" id="telefone" name="telefone" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="qDias">Dias Alugados:</label>';
+          echo '<input type="number" id="qDias" name="qDias" min="1" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="dRetirada">Data Retirada:</label>';
+          echo '<input type="date" id="dRetirada" name="dRetirada" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="dDevolucao">Data Devolução:</label>';
+          echo '<input type="date" id="dDevolucao" name="dDevolucao" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="lRetirada">Local Retirada:</label>';
+          echo '<input type="text" id="lRetirada" name="lRetirada" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="lDevolucao">Local Devolução:</label>';
+          echo '<input type="text" id="lDevolucao" name="lDevolucao" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="hRetirada">Hora Retirada:</label>';
+          echo '<input type="time" id="hRetirada" name="hRetirada" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="hDevolucao">Hora Devolução:</label>';
+          echo '<input type="time" id="hDevolucao" name="hDevolucao" required>';
+          echo '</div>';
+          echo '<div class="campo">';
+          echo '<label for="carro">Carro Escolhido:</label>';
+          echo '<select id="carro" name="carro" required>';
+          echo '<option value="">Selecione...</option>';
+          echo '<option value="Fiat Argo">Fiat Argo</option>';
+          echo '<option value="Chevrolet Onix Plus">Chevrolet Onix Plus</option>';
+          echo '<option value="Volkswagen Nivus">Volkswagen Nivus</option>';
+          echo '<option value="Jeep Renegade">Jeep Renegade</option>';
+          echo '</select>';
+          echo '</div>';
+          echo '<button class="btnsOK" type="submit" onclick="closeModal()">Alugar</button>';
+          echo '</form>';
+          echo '</div>';
+        }
+        ?>
+      </div>
   </nav>
+  <script src="./js/modal.js"></script>
+
+</body>
+
+</html>
